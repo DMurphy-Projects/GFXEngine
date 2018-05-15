f7f6b014cab2a3f29d17cbd30bc7e1242d808b53
Dean-PC\Dean <dmurphy1994@hotmail.co.uk>
2018-05-15 18:30:24 +0100

    fixed bug where text output was creating too much ovoerhead when not in use.

--- src/DebugTools/TextOutput.java
+++ src/DebugTools/TextOutput.java
@@ -15,15 +15,53 @@ public class TextOutput {
     {
         MINIMAL,
         NORMAL,
-        EXTENDED
+        EXTENDED,
+        REMOVED
     }
     private static Mode current;
+
+    static class Method
+    {
+        void println(Object o, int cat, int pos)
+        {
+            StackTraceElement e = Thread.currentThread().getStackTrace()[pos];
+            String c = e.getClassName();
+            String s = o.toString();
+            if (module.allow(c, cat)) {
+                if (current.equals(TextOutput.Mode.NORMAL))
+                {
+                    System.out.println(c + " - " + s);
+                }
+                else if (current.equals(TextOutput.Mode.MINIMAL)) {
+                    if (!lastClass.equals(c))
+                    {
+                        lastClass = c;
+                        System.out.println(c);
+                    }
+                    System.out.println("    " + s);
+                }
+                else if (current.equals(TextOutput.Mode.EXTENDED))
+                {
+                    System.out.println(String.format("%s in %s(line %s): %s", c, e.getMethodName(), e.getLineNumber(), s));
+                }
+            }
+        }
+    }
+
+    private static class NullMethod extends Method
+    {
+        @Override
+        void println(Object o, int cat, int pos) {
+        }
+    }
+    private static Method currentMethod;
 
     static
     {
         //the least amount of performance impact with this setup
         module = new NullImplement();
         current = Mode.NORMAL;
+        currentMethod = new Method();
     }
 
     public static void setModule(BaseTextModule m)
@@ -32,6 +70,14 @@ public class TextOutput {
     }
     public static void setMode(Mode m) {
         current = m;
+        if (current == Mode.REMOVED)
+        {
+            currentMethod = new NullMethod();
+        }
+        else
+        {
+            currentMethod = new Method();
+        }
     }
 
     public static void println(Object o)
@@ -46,26 +92,6 @@ public class TextOutput {
 
     private static void println(Object o, int cat, int pos)
     {
-        StackTraceElement e = Thread.currentThread().getStackTrace()[pos];
-        String c = e.getClassName();
-        String s = o.toString();
-        if (module.allow(c, cat)) {
-            if (current.equals(Mode.NORMAL))
-            {
-                System.out.println(c + " - " + s);
-            }
-            else if (current.equals(Mode.MINIMAL)) {
-                if (!lastClass.equals(c))
-                {
-                    lastClass = c;
-                    System.out.println(c);
-                }
-                System.out.println("    " + s);
-            }
-            else if (current.equals(Mode.EXTENDED))
-            {
-                System.out.println(String.format("%s in %s(line %s): %s", c, e.getMethodName(), e.getLineNumber(), s));
-            }
-        }
+        currentMethod.println(o, cat, pos);
     }
 }
