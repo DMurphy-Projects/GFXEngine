1393b33246b39a11628a42317360b94fcffffc59
Dean-PC\Dean <dmurphy1994@hotmail.co.uk>
2019-03-05 20:04:30 +0000

    reduced number of arrays required for the pre-calculation. since this change has removed some meaning from the code, included what pre-calc elements represent and calculation of weights.

--- src/main/java/Programs/NSidedPolyBaryTest.java
+++ src/main/java/Programs/NSidedPolyBaryTest.java
@@ -18,49 +18,44 @@ public class NSidedPolyBaryTest {
 
         double[] point = new double[]{.5, .5};
 
-        int numberOfPossibleVectors = poly.length-2;
+        int noTriangles = poly.length - 2;
+        double[][] vectors = new double[poly.length-1][2];
 
-        double[][] v0 = new double[numberOfPossibleVectors][2];
-        double[][] v1 = new double[numberOfPossibleVectors][2];
-
-        double[] dot00 = new double[numberOfPossibleVectors];
-        double[] dot01 = new double[numberOfPossibleVectors];
-        double[] dot11 = new double[numberOfPossibleVectors];
-
-        double[] invDenom = new double[numberOfPossibleVectors];
+        int size = 4;//how many arrays do we want to combine
+        double[] preCalc = new double[noTriangles * size];//technically not just dot products anymore
 
         double[] weights = new double[3];
         int[] triangleIndex = new int[3];
 
-        //pre computed values
-        int noTriangles = poly.length - 2;
+        //---pre computed values start
+        for (int i=0;i<poly.length-1;i++)
+        {
+            vectors[i] = VectorCalc.sub(poly[i+1], poly[0]);//all vectors towards poly[0], this is both (C-A, B-A)
+        }
+
         for (int i=0;i<noTriangles;i++)
         {
-            //A is 0, B is i1, C is i2
-            v0[i] = VectorCalc.sub(poly[i+2], poly[0]);//C-A
-            v1[i] = VectorCalc.sub(poly[i+1], poly[0]);//B-A
-
-            dot00[i] = VectorCalc.dot(v0[i], v0[i]);
-            dot01[i] = VectorCalc.dot(v0[i], v1[i]);
-            dot11[i] = VectorCalc.dot(v1[i], v1[i]);
-
-            invDenom[i] = (dot00[i] * dot11[i]) - (dot01[i] * dot01[i]);
+            preCalc[i*size] = VectorCalc.dot(vectors[i+1], vectors[i+1]);//dot00
+            preCalc[i*size+1] = VectorCalc.dot(vectors[i+1], vectors[i]);//dot01
+            preCalc[i*size+2] = VectorCalc.dot(vectors[i], vectors[i]);//dot11
+            preCalc[i*size+3] = (preCalc[i*size] * preCalc[i*size+2]) - (preCalc[i*size+1] * preCalc[i*size+1]);//denominator
         }
+        //---pre computed values end
 
         //calc if point is in polygon, and which one
         for (int i=0;i<noTriangles;i++)
         {
             double[] v2 = VectorCalc.sub(point, poly[0]);//P-A
-            double dot02 = VectorCalc.dot(v0[i], v2);
-            double dot12 = VectorCalc.dot(v1[i], v2);
+            double dot02 = VectorCalc.dot(vectors[i+1], v2);
+            double dot12 = VectorCalc.dot(vectors[i], v2);
 
-            double v = ((dot00[i] * dot12) - (dot01[i] * dot02)) / invDenom[i];
-            double w = ((dot11[i] * dot02) - (dot01[i] * dot12)) / invDenom[i];
+            double v = ((preCalc[i*size] * dot12) - (preCalc[i*size+1] * dot02)) / preCalc[i*size+3];//(dot00*dot12 - dot01*dot02) / denom
+            double w = ((preCalc[i*size+2] * dot02) - (preCalc[i*size+1] * dot12)) / preCalc[i*size+3];//(dot11*dot02) - (dot01*dot12) / denom
 
             //test if inside triangle
             if ((w >= 0) && (v >= 0) && (w + v < 1))
             {
-                weights[0] = 1 - w - v;
+                weights[0] = 1 - w - v;//u
                 weights[1] = v;
                 weights[2] = w;
 
