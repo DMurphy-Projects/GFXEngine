f380ec9d46497c60c9b150c93238b684e8c45415
Dean-PC\Dean <dmurphy1994@hotmail.co.uk>
2019-03-06 17:50:03 +0000

    created a kernel to test using inverse matrix along side the work from NSidedPolyBaryTest.

---
+++ src/main/java/TextureGraphics/BarycentricGpuRender_v4.java
@@ -0,0 +1,318 @@
+package TextureGraphics;
+
+import GxEngine3D.Helper.ArrayHelper;
+import GxEngine3D.Helper.PolygonClipBoundsChecker;
+import GxEngine3D.Helper.VectorCalc;
+import TextureGraphics.Memory.AsyncJoclMemory;
+import TextureGraphics.Memory.JoclMemory;
+import TextureGraphics.Memory.Texture.ITexture;
+import org.jocl.Pointer;
+import org.jocl.Sizeof;
+import org.jocl.cl_event;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.jocl.CL.*;
+import static org.jocl.CL.CL_MEM_READ_WRITE;
+import static org.jocl.CL.clSetKernelArg;
+
+//could be optimised further
+//does not have the sparse pixel problem of predecessors however this is not robust against the flaws in the projection matrix
+public class BarycentricGpuRender_v4 extends JoclRenderer{
+
+    int screenWidth, screenHeight;
+
+    BufferedImage image;
+
+    double[] zMapStart;
+
+    ArrayList<cl_event> taskEvents;
+
+    //names to retrieve arguments by
+    String pixelOut = "Out1", zMapOut = "Out2", screenSize = "ScreenSize";
+
+    //arg indices
+    int screenSizeArg = 0,
+            texureArg = 1,
+            textureSizeArg = 2,
+            noPolygonsArg =3,
+            relativeSpaceStart = 4,//uses 3 indices
+            clipSpaceStart = 7,//uses 3 indices
+            screenSpaceStart = 10,//uses 3 indices
+            textureAnchorStart = 13,//uses 2 indices
+            outArg = 15,
+            zMapArg =16,
+            planeEqArg = 17,
+            offsetArg = 18,
+            inverseMatrixArg = 19,
+            preCalcArg = 20
+    ;
+
+    int color = Color.WHITE.getRGB();
+
+    public BarycentricGpuRender_v4(int screenWidth, int screenHeight)
+    {
+        this.profiling = true;
+
+        this.screenWidth = screenWidth;
+        this.screenHeight = screenHeight;
+
+        create("resources/Kernels/TextureRenderPolyBary.cl", "drawTriangle");
+
+        super.start();
+
+        zMapStart = new double[screenWidth*screenHeight];
+        Arrays.fill(zMapStart, 1);
+    }
+
+    @Override
+    protected void initStaticMemory() {
+        super.initStaticMemory();
+
+        immutable.put(null, screenSize, new int[]{screenWidth, screenHeight}, CL_MEM_READ_ONLY);
+        setupScreenSizeArgs();
+    }
+
+    @Override
+    public void setup() {
+        super.setup();
+        taskEvents = new ArrayList<>();
+        image = new BufferedImage(screenWidth, screenHeight, BufferedImage.TYPE_INT_RGB);
+        setupOutputMemory();
+    }
+
+    //converts from the form {{x, y, z}, ...} into {{x0, x1, ...}, {y0, y1, ...}, ...}
+    private double[][] flip(double[][] array)
+    {
+        double[][] newArray = new double[array[0].length][array.length];
+
+        for (int i=0;i<array.length;i++)
+        {
+            for (int ii=0;ii<array[0].length;ii++) {
+                newArray[ii][i] = array[i][ii];
+            }
+        }
+
+        return newArray;
+    }
+
+    double[] inverseMatrix;
+    public void setInverseMatrix(double[] i)
+    {
+        inverseMatrix = i;
+    }
+
+    double[][] relativePoly;
+    public void setRelativePoly(double[][] relativePoly)
+    {
+        this.relativePoly = relativePoly;
+    }
+
+
+    @Override
+    public void render(double[][] polygon, double[][] textureAnchor, ITexture texture) {
+        if (PolygonClipBoundsChecker.shouldCull(polygon)) return;
+        setupTextureArgs(texture);
+
+        double[][] screenPoly = new double[polygon.length][polygon[0].length];
+        for (int i=0;i<polygon.length;i++)
+        {
+            //change from clip-space to screen-space but preserve z value
+            screenPoly[i][0] = (int) ((polygon[i][0] + 1) * 0.5 * screenWidth);
+            screenPoly[i][1] = (int) ((1 - (polygon[i][1] + 1) * 0.5) * screenHeight);
+            screenPoly[i][2] = polygon[i][2];
+        }
+
+        double[][] preComputeVec = preComputeVectors(relativePoly);
+        double[] preComputeValues = preComputeValeus(preComputeVec, relativePoly.length-2);
+        double[] preComputeVecFlat = ArrayHelper.flatten(preComputeVec);
+
+        //calc bounds for triangle
+        Polygon p = new Polygon();
+        for (double[] point:screenPoly)
+        {
+            p.addPoint((int)point[0], (int) point[1]);
+        }
+        Rectangle r = p.getBounds();
+        r = r.intersection(new Rectangle(0, 0, screenWidth, screenHeight));
+
+        double[][] relativePoly = flip(this.relativePoly);
+        double[][] clipPoly = flip(polygon);
+        screenPoly = flip(screenPoly);
+        double[][] tAnchor = flip(textureAnchor);
+
+        cl_event task = renderPolygon(relativePoly, clipPoly, screenPoly, tAnchor, preComputeVecFlat, preComputeValues,
+                new int[]{r.x, r.y}, Math.max(r.getWidth(), 1), Math.max(r.getHeight(), 1));
+        taskEvents.add(task);
+    }
+
+    private double[][] preComputeVectors(double[][] poly)
+    {
+        double[][] vectors = new double[poly.length-1][];
+        for (int i=0;i<poly.length-1;i++)
+        {
+            vectors[i] = VectorCalc.sub(poly[i+1], poly[0]);//all vectors towards poly[0], this is both (C-A, B-A)
+        }
+
+        return vectors;
+    }
+
+    private double[] preComputeValeus(double[][] vectors, int noTriangles)
+    {
+        int size = 4;
+        double[] preCalc = new double[noTriangles * size];
+        for (int i=0;i<noTriangles;i++)
+        {
+            preCalc[i*size] = VectorCalc.dot(vectors[i+1], vectors[i+1]);//dot00
+            preCalc[i*size+1] = VectorCalc.dot(vectors[i+1], vectors[i]);//dot01
+            preCalc[i*size+2] = VectorCalc.dot(vectors[i], vectors[i]);//dot11
+            preCalc[i*size+3] = (preCalc[i*size] * preCalc[i*size+2]) - (preCalc[i*size+1] * preCalc[i*size+1]);//denominator
+        }
+        return preCalc;
+    }
+
+    private cl_event renderPolygon(double[][] relativePoly, double[][] clipPoly, double[][] screenPoly, double[][] textureAnchor, double[] preCalcVectors, double[] preCalcValues,
+                                   int[] offset, double width, double height)
+    {
+        //(tX, tY, tZ) should have the same length
+        clSetKernelArg(kernel, noPolygonsArg, Sizeof.cl_int, Pointer.to(new int[]{relativePoly[0].length}));
+
+        cl_event taskEvent = new cl_event();
+        cl_event[] relativeEvents = setupPolygonArgs(relativeSpaceStart, taskEvent, relativePoly[0], relativePoly[1], relativePoly[2]);
+        cl_event[] clipEvents = setupPolygonArgs( clipSpaceStart, taskEvent, clipPoly[0], clipPoly[1], clipPoly[2]);
+        cl_event[] screenEvents = setupPolygonArgs(screenSpaceStart, taskEvent, screenPoly[0], screenPoly[1], screenPoly[2]);
+        cl_event[] textureAnchorEvents = setupPolygonArgs(textureAnchorStart, taskEvent, textureAnchor[0], textureAnchor[1]);
+        cl_event[] inverseMatrixEvents = setupPolygonArgs(inverseMatrixArg, taskEvent, inverseMatrix);
+        cl_event[] preCalcEvents = setupPolygonArgs(preCalcArg, taskEvent, preCalcVectors, preCalcValues);
+
+
+        cl_event eqEvent = setupPlaneEqaution(screenPoly[0], screenPoly[1], screenPoly[2], taskEvent);
+        cl_event offEvent = setupOffset(offset, taskEvent);
+
+        int arrayPos = 0;
+        cl_event[] waitingEvents = new cl_event[relativeEvents.length + clipEvents.length + screenEvents.length + textureAnchorEvents.length + inverseMatrixEvents.length + preCalcEvents.length + 2];
+        waitingEvents[0] = eqEvent;
+        waitingEvents[1] = offEvent;
+        System.arraycopy(relativeEvents, 0, waitingEvents, arrayPos+=2, relativeEvents.length);//moves relativeEvents into waitingEvents
+        System.arraycopy(clipEvents, 0, waitingEvents, arrayPos+=relativeEvents.length, clipEvents.length);//moves clipEvents into waitingEvents
+        System.arraycopy(screenEvents, 0, waitingEvents, arrayPos+=clipEvents.length, screenEvents.length);//moves screenEvents into waitingEvents
+        System.arraycopy(textureAnchorEvents, 0, waitingEvents, arrayPos+=screenEvents.length, textureAnchorEvents.length);//moves textureAnchor into waitingEvents
+        System.arraycopy(inverseMatrixEvents, 0, waitingEvents, arrayPos+=textureAnchorEvents.length, inverseMatrixEvents.length);//moves inverseMatrix into waitingEvents
+        System.arraycopy(preCalcEvents, 0, waitingEvents, arrayPos+=inverseMatrixEvents.length, preCalcEvents.length);//moves preCalcEvents into waitingEvents
+
+        long[] globalWorkSize = new long[] {
+                (long) width,
+                (long) height
+        };
+
+        clEnqueueNDRangeKernel(commandQueue, kernel, 2, null,
+                globalWorkSize, null, waitingEvents.length, waitingEvents, taskEvent);
+
+        return taskEvent;
+    }
+
+    @Override
+    public BufferedImage createImage() {
+        //a taskEvents size of 0 means that we culled all triangles from the render, so we don't need to read the data
+        if (taskEvents.size() > 0) {
+
+            DataBufferInt dataBuffer = (DataBufferInt) image.getRaster().getDataBuffer();
+            int data[] = dataBuffer.getData();
+            cl_event[] events = new cl_event[taskEvents.size()];
+            taskEvents.toArray(events);
+
+            clWaitForEvents(events.length, events);
+            readData(data);
+
+            ExecutionStatistics stats = new ExecutionStatistics();
+            for (cl_event event:taskEvents)
+            {
+                stats.addEntry("", event, ExecutionStatistics.Formats.Nano);
+            }
+            stats.printTotal();
+        }
+
+        finish();
+        return image;
+    }
+
+    //JOCL handling functions
+    private void readData(int[] out)
+    {
+        //read image
+        clEnqueueReadBuffer(commandQueue, dynamic.get(pixelOut).getRawObject(), CL_TRUE, 0,
+                Sizeof.cl_uint * screenWidth*screenHeight, Pointer.to(out), 0, null, null);
+    }
+
+    private void setupScreenSizeArgs()
+    {
+        clSetKernelArg(kernel, screenSizeArg, Sizeof.cl_mem, immutable.get(screenSize).getObject());
+    }
+
+    //OUTPUT ARGUMENTS
+    private void setupOutputMemory()
+    {
+        recreateOutputMemory(screenWidth*screenHeight);
+        setupOutArgs();
+    }
+
+    private void recreateOutputMemory(int size)
+    {
+        dynamic.put(pixelOut, size * Sizeof.cl_int, CL_MEM_WRITE_ONLY);
+        dynamic.put(null, zMapOut, zMapStart, CL_MEM_READ_WRITE);
+    }
+
+    private void setupOutArgs()
+    {
+        clSetKernelArg(kernel, outArg, Sizeof.cl_mem, dynamic.get(pixelOut).getObject());
+        clSetKernelArg(kernel, zMapArg, Sizeof.cl_mem, dynamic.get(zMapOut).getObject());
+    }
+    //OUTPUT ARGUMENT END
+
+    private cl_event setupOffset(int[] offset, cl_event task)
+    {
+        JoclMemory m = dynamic.put(task, null, offset, CL_MEM_READ_ONLY);
+        clSetKernelArg(kernel, offsetArg, Sizeof.cl_mem, m.getObject());
+        return ((AsyncJoclMemory)m).getFinishedWritingEvent();
+    }
+
+    private cl_event setupPlaneEqaution(double[] tX, double[] tY, double[] tZ, cl_event task)
+    {
+        double[] v1 = new double[]{tX[1] - tX[0], tY[1] - tY[0], tZ[1] - tZ[0]};
+        double[] v2 = new double[]{tX[2] - tX[0], tY[2] - tY[0], tZ[2] - tZ[0]};
+        double[] normalVector = VectorCalc.cross(v1, v2);
+
+        double[] planeEq = VectorCalc.plane_v3_pointForm(normalVector, new double[]{tX[0], tY[0], tZ[0]});
+
+        JoclMemory m = dynamic.put(task, null, planeEq, CL_MEM_READ_ONLY);
+        clSetKernelArg(kernel, planeEqArg, Sizeof.cl_mem, m.getObject());
+        return ((AsyncJoclMemory)m).getFinishedWritingEvent();
+    }
+
+    private void setupTextureArgs(ITexture texture)
+    {
+        clSetKernelArg(kernel, texureArg, Sizeof.cl_mem, texture.getTexture());
+        clSetKernelArg(kernel, textureSizeArg, Sizeof.cl_mem, texture.getSize());
+    }
+
+    private cl_event[] setupPolygonArgs(int start, cl_event task, double[]... data)
+    {
+        cl_event[] events = new cl_event[data.length];
+        JoclMemory m;
+        int index = 0;
+
+        for (double[] d: data)
+        {
+            m = dynamic.put(task, null, d, CL_MEM_READ_ONLY);
+            events[index++] = ((AsyncJoclMemory)m).getFinishedWritingEvent();
+            clSetKernelArg(kernel, start++, Sizeof.cl_mem, m.getObject());
+        }
+
+        return events;
+    }
+}
---
+++ src/main/resources/Kernels/TextureRenderPolyBary.cl
@@ -0,0 +1,135 @@
+//this is an implementation for drawing a triangle with a solid color
+
+//function declaration
+
+bool isOutOfBounds(double x, double y, __global int *screenSize);
+void applyMatrix(__global double *matrix, double *in, double *out);
+double dot_v3(double v1X, double v1Y, double v1Z, double *v2);
+int getTextureAt(int *texturePoint, __global int *textureSize, __global uint *texture);
+
+//function end
+
+//note:
+//noPolygons:   the number of vertices in the polygon
+//p?X, p?Y, p?Z: contains the (x, y, z) values of the polygon to render, ? is which space the poly resides in (R: relative, C: clip, S: screen)
+//out:          a pixel grid reduced to 1 dimension
+//zMap:         a grid of depth values reduced to 1 dimension
+//planeEq:      [a, b, c, d] of the form, ax + by + cz + d = 0
+//offset:       this kernel iterates over a rectangle region of the screen, this value moves that region
+//inverse:      the inverse matrix which goes from clip-space -> relative-space for this polygon
+//vectors:      contains the vectors required to calculate the barycentric co-ords in relative-space
+//preCalc:      contains information, other than vectors, to calculate barycentric co-ords in relative-space
+__kernel void drawTriangle(
+    __global int *screenSize,
+    __global uint *texture, __global int *textureSize,
+    int noPolygons,
+    __global double *pRX, __global double *pRY, __global double *pRZ,
+    __global double *pCX, __global double *pCY, __global double *pCZ,
+    __global double *pSX, __global double *pSY, __global double *pSZ,
+    __global double *tAX, __global double *tAY,
+    __global uint *out, __global double *zMap,
+    __global double *planeEq, __global int *offset,
+    __global double *inverse,
+    __global double *vectors, __global double *preCalc
+    )
+{
+    double x = (get_local_size(0) * get_group_id(0) + get_local_id(0)) + offset[0];
+    double y = (get_local_size(1) * get_group_id(1) + get_local_id(1)) + offset[1];
+
+    //http://www.ecse.rpi.edu/Homepages/wrf/Research/Short_Notes/pnpoly.html
+    bool inside = false;
+    for ( int i = 0, j = noPolygons - 1 ; i < noPolygons ; j = i++ )
+    {
+        if ( ( pSY[i] > y ) != ( pSY[j] > y ) &&
+            x < ( pSX[j] - pSX[i] ) * ( y - pSY[i] ) / ( pSY[j] - pSY[i] ) + pSX[i] )
+        {
+            inside = !inside;
+        }
+    }
+    if (inside)
+    {
+        int pos = (y * screenSize[0]) + x;
+
+        //we need to find z value, by using plane eq. ax + by +cz + d = 0, we can find (a, b, c, d) using triangle points
+        //then substitute (x, y) and re-arrange for z, thus
+        //z = (-ax - by - d) / c
+        double z = (-(planeEq[0]*x) - (planeEq[1]*y) - planeEq[3]) / planeEq[2];
+        if (z < 1 && z > 0 && zMap[pos] > z)
+        {
+            zMap[pos] = z;
+            //calculate clip-space for (x, y), z is already in clip
+            //screen_x = (clip_x + 1) * 0.5 * screenWidth
+            //(screen_x / (screenWidth * 0.5)) - 1 = clip_x
+            double clip_x = (x / (screenSize[0] * 0.5)) - 1;
+
+            //screen_y =  (1 - (clip_y + 1)* 0.5) * screenHeight
+            //(2 * (1 - (screen_y / screenHeight))) - 1 = clip_y
+            double clip_y = (2 * (1 - (y / screenSize[1]))) - 1;
+
+
+            //apply inverse to (x, y) to get relative-space points
+            double p_in[3] = {clip_x, clip_y, z};
+            double p_out[3] = {0, 0, 0};
+
+            applyMatrix(inverse, p_in, p_out);//use p_in, write into p_out
+
+            //find barycentric co-ords for relative space points
+            int color = 0;
+            int size = 4;
+            for (int i=0;i < noPolygons-2;i++)
+            {
+                double v2[] = {p_out[0]-pRX[0], p_out[1]-pRY[0], p_out[2]-pRZ[0]};//P-A
+                double dot02 = dot_v3(vectors[(i+1)*3], vectors[(i+1)*3+1], vectors[(i+1)*3+2], v2);
+                double dot12 = dot_v3(vectors[i*3], vectors[i*3+1], vectors[i*3+2], v2);
+
+                double v = ((preCalc[i*size] * dot12) - (preCalc[i*size+1] * dot02)) / preCalc[i*size+3];//(dot00*dot12 - dot01*dot02) / denom
+                double w = ((preCalc[i*size+2] * dot02) - (preCalc[i*size+1] * dot12)) / preCalc[i*size+3];//(dot11*dot02) - (dot01*dot12) / denom
+
+                //test if inside triangle
+                if ((w >= 0) && (v >= 0) && (w + v < 1))
+                {
+                    double u = 1 - w - v;//u
+
+                    int texturePoint[2] = {
+                        (int)(textureSize[0] * ((u * tAX[0]) + (v * tAX[i+1]) + (w * tAX[i+2]))),
+                        (int)(textureSize[1] * ((u * tAY[0]) + (v * tAY[i+1]) + (w * tAY[i+2])))
+                    };
+                    //map to texture space to get color
+                    color = getTextureAt(texturePoint, textureSize, texture);
+                    out[pos] = color;
+                    return;
+                }
+            }
+
+        }
+    }
+}
+
+void applyMatrix(__global double *matrix, double *in, double *out)
+{
+    double slice = in[0] * matrix[12] + in[1] * matrix[13] + in[2] * matrix[14] + matrix[15];
+    slice = fabs(slice);
+
+    out[0] = in[0] * matrix[0] + in[1] * matrix[1] + in[2] * matrix[2] +  matrix[3];
+    out[1] = in[0] * matrix[4] + in[1] * matrix[5] + in[2] * matrix[6] +  matrix[7];
+    out[2] = in[0] * matrix[8] + in[1] * matrix[9] + in[2] * matrix[10] +  matrix[11];
+
+    out[0] = out[0] / slice;
+    out[1] = out[1] / slice;
+    out[2] = out[2] / slice;
+}
+
+double dot_v3(double v1X, double v1Y, double v1Z, double *v2)
+{
+   return (v1X* v2[0]) + (v1Y*v2[1]) + (v1Z*v2[2]);
+}
+
+int getTextureAt(int *texturePoint, __global int *textureSize, __global uint *texture)
+{
+    if (texturePoint[0] < 0 || texturePoint[1] < 0 || texturePoint[0] >= textureSize[0] || texturePoint[1] >= textureSize[1])
+    {
+        return (255 << 16) | (255 << 8 ) | 255;
+    }
+    int index = (texturePoint[1] * textureSize[0]) + texturePoint[0];
+    return texture[index];
+}
