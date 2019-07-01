6a63610f64607396d9e8dbebec7812dee5b4f37d
Dean-PC\Dean <dmurphy1994@hotmail.co.uk>
2019-07-01 13:09:42 +0100

    added ability to have shared context with multiple JoclPrograms

--- src/main/java/TextureGraphics/JoclProgram.java
+++ src/main/java/TextureGraphics/JoclProgram.java
@@ -74,14 +74,9 @@ public abstract class JoclProgram {
         cleanupThread.start();
     }
 
-    protected void create(String sourceFile, String kernalProgram)
+    public static cl_platform_id createPlatform()
     {
         final int platformIndex = 0;
-        final long deviceType = CL_DEVICE_TYPE_ALL;
-        final int deviceIndex = 0;
-
-        // Enable exceptions and subsequently omit error checks in this sample
-        CL.setExceptionsEnabled(true);
 
         // Obtain the number of platforms
         int numPlatformsArray[] = new int[1];
@@ -93,9 +88,13 @@ public abstract class JoclProgram {
         clGetPlatformIDs(platforms.length, platforms, null);
         cl_platform_id platform = platforms[platformIndex];
 
-        // Initialize the context properties
-        cl_context_properties contextProperties = new cl_context_properties();
-        contextProperties.addProperty(CL_CONTEXT_PLATFORM, platform);
+        return platform;
+    }
+
+    public static cl_device_id createDevice(cl_platform_id platform)
+    {
+        final long deviceType = CL_DEVICE_TYPE_ALL;
+        final int deviceIndex = 0;
 
         // Obtain the number of devices for the platform
         int numDevicesArray[] = new int[1];
@@ -107,19 +106,60 @@ public abstract class JoclProgram {
         clGetDeviceIDs(platform, deviceType, numDevices, devices, null);
         cl_device_id device = devices[deviceIndex];
 
+        return device;
+    }
+
+    public static cl_context createContext(cl_device_id device, cl_platform_id platform)
+    {
+        // Enable exceptions and subsequently omit error checks in this sample
+        CL.setExceptionsEnabled(true);
+
+        // Initialize the context properties
+        cl_context_properties contextProperties = new cl_context_properties();
+        contextProperties.addProperty(CL_CONTEXT_PLATFORM, platform);
+
         // Create a context for the selected device
-        context = clCreateContext(
+        cl_context context = clCreateContext(
                 contextProperties, 1, new cl_device_id[]{device},
                 null, null, null);
 
+        return context;
+    }
+
+    public static cl_command_queue createCommandQueue(cl_context context, cl_device_id device, boolean profiling)
+    {
         cl_queue_properties p = new cl_queue_properties();
         if (profiling) {
             p.addProperty(CL_QUEUE_PROPERTIES, CL_QUEUE_PROFILING_ENABLE);
-            this.device = device;
         }
 
-        commandQueue = clCreateCommandQueueWithProperties(context, device, p, null);
+        cl_command_queue commandQueue = clCreateCommandQueueWithProperties(context, device, p, null);
 
+        return commandQueue;
+    }
+
+    protected void create(String sourceFile, String kernalProgram, JoclProgram sharedProgram)
+    {
+        this.profiling = sharedProgram.profiling;
+        create(sourceFile, kernalProgram, sharedProgram.context, sharedProgram.commandQueue);
+    }
+
+    protected void create(String sourceFile, String kernalProgram)
+    {
+        cl_platform_id platform = createPlatform();
+        cl_device_id device = createDevice(platform);
+
+        cl_context context = createContext(device, platform);
+
+        cl_command_queue commandQueue = createCommandQueue(context, device, this.profiling);
+
+        create(sourceFile, kernalProgram, context, commandQueue);
+    }
+
+    private void create(String sourceFile, String kernalProgram, cl_context context, cl_command_queue commandQueue)
+    {
+        this.context = context;
+        this.commandQueue = commandQueue;
         // Program Setup
         String source = readFile(sourceFile);
 
